4ec9d1f773fba9d0c14c73ef4048b59f3c986a02
Yuri Gonçalves <111998370+Yurig19@users.noreply.github.com>
2023-12-21 20:10:30 -0300

    feat: creating user table migration

--- knexfile.js
+++ knexfile.js
@@ -6,14 +6,14 @@ module.exports = {
 		client: 'sqlite3',
 		connection: {
 			filename: path.resolve(__dirname, 'src', 'database', 'database.db')
-		}
+		},
+		pool: {
+			afterCreate: (conn, cb) => conn.run('PRAGMA foreign_keys = ON', cb) 
+		},
+		migrations: {
+			directory: path.resolve(__dirname, 'src', 'database', 'migrations')
+		},
+		useNullAsDefault: true
 	},
-	pool: {
-		afterCreate: (conn, cb) => conn.run('PRAGMA foreign_key = ON', cb) 
-	},
-	migrations: {
-		directory: path.resolve(__dirname, 'src', 'database', 'migrations')
-	},
-	useNullAsDefault: true
 };
 
--- src/controllers/UsersControllers.js
+++ src/controllers/UsersControllers.js
@@ -1,4 +1,5 @@
 class UsersControllers {
 }
 
+
 module.exports = UsersControllers;
