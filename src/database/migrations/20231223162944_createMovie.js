9372e925584179ddd45e6887d30724074c1228de
Yuri Gonçalves <111998370+Yurig19@users.noreply.github.com>
2023-12-26 12:13:28 -0300

    feat: creating movies table migration

---
+++ src/database/migrations/20231223162944_createMovie.js
@@ -0,0 +1,13 @@
+
+exports.up = knex => knex.schema.createTable('movies', table => {
+	table.increments('id');
+	table.text('name').notNullable();
+	table.text('description').notNullable();
+	table.integer('rating');
+	table.integer('user_id').references('id').inTable('users');
+	table.timestamp('created_at').default(knex.fn.now());
+	table.timestamp('updated_at').default(knex.fn.now());
+});
+
+
+exports.down =  knex => knex.schema.dropTable('movies');
